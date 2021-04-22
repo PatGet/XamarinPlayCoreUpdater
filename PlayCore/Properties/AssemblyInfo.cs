ba129f57e0ea853a5f6cf809ba694133eeb37741
Patrick Getzmann <pgetzmann@hotmail.de>
2021-04-22 20:42:32 +0200

    Updated NuSpec after package creation

--- PlayCore/Properties/AssemblyInfo.cs
+++ PlayCore/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
+[assembly: AssemblyVersion("1.10.0.0")]
+[assembly: AssemblyFileVersion("1.10.0.0")]
