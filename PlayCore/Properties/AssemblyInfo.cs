19c03fc177ba48296cfe75a57c6bb84e43d574b3
Saamer Mansoor <i@saamer.me>
2021-10-16 14:51:55 -0400

    upgrades/1.10.2-NewAAR (#21)
    * Replaced the old AAR library with the new one

--- PlayCore/Properties/AssemblyInfo.cs
+++ PlayCore/Properties/AssemblyInfo.cs
@@ -22,5 +22,5 @@ using Android.App;
 //      Minor Version 
 //      Build Number
 //      Revision
-[assembly: AssemblyVersion("1.10.0.0")]
-[assembly: AssemblyFileVersion("1.10.0.0")]
+[assembly: AssemblyVersion("1.10.2.0")]
+[assembly: AssemblyFileVersion("1.10.2.0")]
